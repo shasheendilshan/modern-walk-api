1979e867227873be014eb5120b82dce7329f0bb8
unknown <shasheen@i-threesixty.com>
2022-07-21 15:00:39 +0530

    Add categories json

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const users = require("./data/users.json");
 const products = require("./data/products.json");
+const categories = require("./data/categories.json");
 
 module.exports = {
   users,
   products,
+  categories,
 };
