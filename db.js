17807e1f8fcce66e3606117e2b0436385dd96b0c
unknown <shasheen@i-threesixty.com>
2022-08-01 11:02:10 +0530

    Add tenants to db

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const users = require("./data/users.json");
 const products = require("./data/products.json");
 const categories = require("./data/categories.json");
+const tenants = require("./data/tenants.json");
 
 module.exports = {
   users,
   products,
   categories,
+  tenants,
 };
